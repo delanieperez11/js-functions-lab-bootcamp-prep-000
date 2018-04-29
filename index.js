a7047d18c0d144e61f213c84322c061e73a44401
Delanie Perez <delanieperez11@gmail.com>
2018-04-29 23:52:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday, name) {
-  return `Happy holidays, ${name}!` 
+  return `Happy ${holiday}, ${name}!`
+}
